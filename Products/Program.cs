48ade2e25881b0326fc8e8fdad6a3dd653435bad
Careless-boop <vityahalushka@gmail.com>
2023-11-17 11:22:23 +0100

    init commit

---
+++ Products/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Products.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ProductsContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(opt => opt
+    .AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .SetIsOriginAllowed(origin => true));
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

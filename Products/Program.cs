b092ac1009370f4319cfa11fdc26597db686af66
Careless-boop <vityahalushka@gmail.com>
2023-11-19 15:14:17 +0100

    Small changes

--- Products/Program.cs
+++ Products/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +16,7 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
